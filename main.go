1fbe8391ede2c361655100f13c10f9507e770020
HorvathAlteisen <horvath_alteisen@gmx.de>
2017-09-20 17:03:14 +0200

    Major Cleanup

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
-import "github.com/HorvathAlteisen/GoTS3Bot/pkg/ts3/query"
-import "github.com/HorvathAlteisen/GoTS3Bot/pkg/ts3/bot"
+import "github.com/HorvathAlteisen/GoTS3Bot/pkg/ts3"
 
 func main() {
 
 	query, _ := ts3.NewQuery("127.0.0.1:10011")
-
-	query
 
 	defer query.Close()
 
