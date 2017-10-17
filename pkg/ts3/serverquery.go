f2ecdd14bdbea8e092637bfb77e915fffa4af8b4
Kevin Pielorz <kpi@stiegeler.intranet>
2017-10-17 10:45:58 +0200

    Updated my Query lib

--- pkg/ts3/serverquery.go
+++ pkg/ts3/serverquery.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"errors"
-	"io"
+	"fmt"
 	"net"
 	"strings"
 	"time"
@@ -12,15 +12,25 @@ import (
 
 // ServerQuery stores TS3 ServerQuery connection details
 type ServerQuery struct {
-	conn    net.Conn
-	cResult chan string
-	rbuf    *bufio.Reader
-	wbuf    *bufio.Writer
-	out     io.Writer
+	conn             net.Conn
+	scan             *bufio.Scanner
+	cLine            chan string
+	cNotify          chan string
+	cErr             chan string
+	res              string
+	notifyHandler    func(string, string)
+	connErrorHandler func(error)
+	WelcomeMsg       string
+}
+
+type Error struct {
+	id  int
+	msg string
 }
 
 // Default Telnet Port of a TS3 Server Query
 const (
+	defaultIP    = "127.0.0.1"
 	defaultPort  = "10011"
 	verifyString = "TS3"
 )
@@ -40,26 +50,92 @@ func NewQuery(address string) (query *ServerQuery, err error) {
 	}
 
 	query.conn, err = net.DialTimeout("tcp", address, DialTimeout)
-	query.rbuf = bufio.NewReader(query.conn)
-	query.wbuf = bufio.NewWriter(query.conn)
-	query.cResult = make(chan string)
-
 	// If the connection fails the funtion returns an error
 	if err != nil {
 		return nil, err
 	}
 
-	line, _ := query.rbuf.ReadString('\n')
+	query.cLine = make(chan string)
 
-	// Checks if the first line contains the signatur "TS3" of the ServerQuery
-	if !strings.Contains(line, verifyString) {
+	query.scan = bufio.NewScanner(query.conn)
+	query.scan.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		/*for b := range data {
 
+			fmt.Println(data[b])
+
+		}
+		fmt.Println("End of Data")*/
+
+		if atEOF && len(data) == 0 {
+			return 0, nil, nil
+		}
+		if i := bytes.Index(data, []byte("\n\r")); i >= 0 {
+			return i + 2, data[0:i], nil
+		}
+		if atEOF {
+			return len(data), data, nil
+		}
+		return 0, nil, nil
+	})
+
+	go func() {
+		for {
+			if !query.scan.Scan() && query.connErrorHandler != nil {
+				err = query.scan.Err()
+				if err != nil {
+					query.connErrorHandler(err)
+				} else {
+					query.connErrorHandler(errors.New("EOF"))
+				}
+			}
+			query.cLine <- query.scan.Text()
+		}
+	}()
+
+	// Checks if the first line contains the signature "TS3" of the ServerQuery
+	if !strings.Contains(<-query.cLine, verifyString) {
+		query.Close()
 		return nil, errors.New("This is not a TS3 Server")
 	}
 
-	line, err = query.rbuf.ReadString('\n')
+	// Get Welcome Msg, dunno I want to show it on my project
+	query.WelcomeMsg = <-query.cLine
 
-	go query.sync()
+	query.cErr = make(chan string)
+	query.cNotify = make(chan string)
+
+	// Notify Handler Loop
+
+	go func() {
+		for {
+			notification := <-query.cNotify
+			fmt.Println(notification) /*
+				if query.notifyHandler != nil {
+					client.notifyHandler(ParseNotification(notification))
+				} else if client.notifyHandlerString != nil {
+					client.notifyHandlerString(notification)
+				} else {
+					// discard
+				}*/
+		}
+	}()
+
+	// Line Handler
+	go func() {
+
+		for {
+			line := <-query.cLine
+
+			if strings.Index(line, "error") == 0 {
+				query.cErr <- line
+			} else if strings.Index(line, "notify") == 0 {
+				query.cNotify <- line
+			} else {
+				query.res = line
+			}
+		}
+
+	}()
 
 	return query, nil
 }
@@ -72,41 +148,11 @@ func (query *ServerQuery) Close() error {
 }
 
 // SendCommand sends query commands to the TS3ServerQuery
-func (query *ServerQuery) SendCommand(command string) (string, error) {
-	command += "\n"
+func (query *ServerQuery) SendCommand(command string) (string, string) {
+	fmt.Fprintf(query.conn, "%s\n\r", command)
+	err := <-query.cErr
+	res := query.res
+	query.res = ""
 
-	_, err := query.send(command)
-
-	return <-query.cResult, err
-}
-
-func (query *ServerQuery) send(p string) (int, error) {
-	b := []byte(p)
-	// Double IAC chars
-	bytes.Replace(b, []byte{0xff}, []byte{0xff, 0xff}, -1)
-	return query.conn.Write(b)
-}
-
-func (query *ServerQuery) Write(p []byte) (int, error) {
-	s := string(p)
-	s = strings.Replace(s, "\r", "", -1)
-
-	query.responseHandler(s)
-
-	//query.handleResponse(s)
-	return len(s), nil
-}
-
-func (query *ServerQuery) responseHandler(data string) {
-
-	query.cResult <- data
-	//lines := strings.Split(data, "\n")
-	//var result string
-}
-
-// cp copies from an io.Reader to an io.Writer
-func (query *ServerQuery) sync() {
-	for {
-		io.Copy(query, query.conn)
-	}
+	return res, err
 }
